1cd7d685d1fc998ada3406bb59e6ff3f1b41c025
Sebastian Arias <juansebastianarias1@gmail.com>
2022-09-11 12:06:11 -0500

    Create tasks
    Receives JSON values from a POST method to insert them into the tasks
    table

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,13 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { con } from "../../../utils/database";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req;
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, body } = req;
 
   switch (method) {
     case "GET":
       return res.status(200).json("Getting tasks");
     case "POST":
-      return res.status(200).json("Posting tasks");
+      const { title, description } = body;
+
+      const query = "INSERT INTO tasks(title, description) VALUES ($1, $2) RETURNING *";
+      const values = [title, description];
+
+      const response = await con.query(query, values);
+
+      return res.status(200).json(response.rows[0]);
     default:
       res.status(400).json("Invalid method");
   }
